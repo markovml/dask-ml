bd83b7afc8136a339f9ee68914088e9140df8eaf
Kushagra Arora <kushagra15049@iiitd.ac.in>
2024-05-22 17:01:12 +0530

    Fix packaging.version usage

--- dask_ml/_compat.py
+++ dask_ml/_compat.py
@@ -6,30 +6,30 @@ from typing import Any
 import dask
 import dask.array as da
 import distributed
-import packaging.version
+import packaging.version as pv
 import pandas
 import sklearn
 import sklearn.utils.validation
 
-SK_VERSION = packaging.version.parse(sklearn.__version__)
-DASK_VERSION = packaging.version.parse(dask.__version__)
-PANDAS_VERSION = packaging.version.parse(pandas.__version__)
-DISTRIBUTED_VERSION = packaging.version.parse(distributed.__version__)
+SK_VERSION = pv.parse(sklearn.__version__)
+DASK_VERSION = pv.parse(dask.__version__)
+PANDAS_VERSION = pv.parse(pandas.__version__)
+DISTRIBUTED_VERSION = pv.parse(distributed.__version__)
 
-DASK_240 = DASK_VERSION >= packaging.version.parse("2.4.0")
-DASK_2130 = DASK_VERSION >= packaging.version.parse("2.13.0")
-DASK_2_20_0 = DASK_VERSION >= packaging.version.parse("2.20.0")
-DASK_2_26_0 = DASK_VERSION >= packaging.version.parse("2.26.0")
-DASK_2_28_0 = DASK_VERSION > packaging.version.parse("2.27.0")
-DASK_2021_02_0 = DASK_VERSION >= packaging.version.parse("2021.02.0")
-DASK_2022_01_0 = DASK_VERSION > packaging.version.parse("2021.12.0")
-DISTRIBUTED_2_5_0 = DISTRIBUTED_VERSION > packaging.version.parse("2.5.0")
-DISTRIBUTED_2_11_0 = DISTRIBUTED_VERSION > packaging.version.parse("2.10.0")  # dev
-DISTRIBUTED_2021_02_0 = DISTRIBUTED_VERSION >= packaging.version.parse("2021.02.0")
-PANDAS_1_2_0 = PANDAS_VERSION > packaging.version.parse("1.2.0")
+DASK_240 = DASK_VERSION >= pv.parse("2.4.0")
+DASK_2130 = DASK_VERSION >= pv.parse("2.13.0")
+DASK_2_20_0 = DASK_VERSION >= pv.parse("2.20.0")
+DASK_2_26_0 = DASK_VERSION >= pv.parse("2.26.0")
+DASK_2_28_0 = DASK_VERSION > pv.parse("2.27.0")
+DASK_2021_02_0 = DASK_VERSION >= pv.parse("2021.02.0")
+DASK_2022_01_0 = DASK_VERSION > pv.parse("2021.12.0")
+DISTRIBUTED_2_5_0 = DISTRIBUTED_VERSION > pv.parse("2.5.0")
+DISTRIBUTED_2_11_0 = DISTRIBUTED_VERSION > pv.parse("2.10.0")  # dev
+DISTRIBUTED_2021_02_0 = DISTRIBUTED_VERSION >= pv.parse("2021.02.0")
+PANDAS_1_2_0 = PANDAS_VERSION > pv.parse("1.2.0")
 WINDOWS = os.name == "nt"
 
-SKLEARN_1_1_X = SK_VERSION >= packaging.version.parse("1.1")
+SKLEARN_1_1_X = SK_VERSION >= pv.parse("1.1")
 
 # 'log_loss' is preferred as of scikit-learn 1.1
 if SKLEARN_1_1_X:
